95044ff95a4b37c144c728a27f5a0ca4dcc4eb38
Filip Kin <me@filipkin.com>
2024-03-06 19:52:11 -0500

    asfkjl;fdaslk

--- js/pictureloop.js
+++ js/pictureloop.js
@@ -1,17 +1,36 @@
-function delay(milliseconds){
+function delay(milliseconds) {
     return new Promise(resolve => {
         setTimeout(resolve, milliseconds);
     });
 }
 
 const pictureloop = async () => {
-    const pics = ['beartear.png'];
+    const pics = [
+        'beartear.png',
+        'terminal-supply.png',
+        'ford.png',
+        'berkley-foundation.png',
+        'mens-club.png',
+        'michigan.png',
+        '3m.png',
+        'automation-ally.png',
+        'bosch.png',
+        'dadra.png',
+        'dodea.png',
+        'raytheon.png',
+        'stellantis.png',
+        'womans-league.png'
+    ];
 
     i = 0;
     while (true) {
         document.getElementById("loop").setAttribute("src", `/img/${pics[i]}`)
-        await delay(5000)
-        
+        if (i < 6) {
+            await delay(6000);
+        } else {
+            await delay(3000);
+        }
+
         i++;
         if (i == pics.length) {
             i = 0;
